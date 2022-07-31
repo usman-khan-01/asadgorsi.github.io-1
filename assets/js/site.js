4d5fc57cd121e278988b73d520f4271e8c0027a7
Usman Khan <53503561+usman-khan-01@users.noreply.github.com>
2022-07-31 21:18:09 +0500

    Done for the day

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,3 +1,4 @@
+// data
 (async () => {
     const response = await fetch('https://graph.perspective-v.com/api/resume', {
         method: 'POST',
@@ -19,6 +20,13 @@
     });
     const body = await response.json();
     var data = JSON.parse(body.data.getbyaccesstoken.jsonData);
-    console.log(data);
-    $('.logo').append(data.fullName);
+    // basic info
+    $('.logo').append(data.fullName + '.');
+    $('.head-trans').append(data.lastName + '.');
+    document.querySelector('.profile-image').src = data.profileImageUrl;
+    document.querySelector('.profile-image').alt = data.fullName + "'s profile Image";
+    $('.text-capitalize').append(data.fullName);
+    $('.profession').append(data.profession);
+    $('.about').append(data.about);
+
 })();
